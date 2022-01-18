02c30c6605e55c968b18d208ff5b88717f859848
Anca Simulei <anca.simuu@gmail.com>
2022-01-18 22:28:53 +0200

    Add project files.

---
+++ Models/Vin.cs
@@ -0,0 +1,11 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace proiect_Anca_Simulei.Models
+{
+    public class Vin
+    {
+    }
+}
